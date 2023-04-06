98fc62bddb4f503db67108e3d257c6cdc26cbec0
shabeeb <shabeebkaip@gmail.com>
2023-04-06 13:41:24 +0530

    project initilialised

---
+++ src/routes.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+// Admin Imports
+import MainDashboard from "views/admin/default";
+import NFTMarketplace from "views/admin/marketplace";
+import Profile from "views/admin/profile";
+import DataTables from "views/admin/tables";
+import RTLDefault from "views/rtl/default";
+
+// Auth Imports
+import SignIn from "views/auth/SignIn";
+
+// Icon Imports
+import {
+  MdHome,
+  MdOutlineShoppingCart,
+  MdBarChart,
+  MdPerson,
+  MdLock,
+} from "react-icons/md";
+
+const routes = [
+  {
+    name: "Dashboard",
+    layout: "/admin",
+    path: "default",
+    icon: <MdHome className="w-6 h-6" />,
+    component: <MainDashboard />,
+  },
+  {
+    name: 'Orders',
+    layout: '/admin',
+    path: 'orders',
+    icon: <MdOutlineShoppingCart className="w-6 h-6" />,
+    component: <NFTMarketplace />,
+    secondary: true,
+
+  },
+
+  {
+    name: "NFT Marketplace",
+    layout: "/admin",
+    path: "nft-marketplace",
+    icon: <MdOutlineShoppingCart className="w-6 h-6" />,
+    component: <NFTMarketplace />,
+    secondary: true,
+  },
+  {
+    name: "Data Tables",
+    layout: "/admin",
+    icon: <MdBarChart className="w-6 h-6" />,
+    path: "data-tables",
+    component: <DataTables />,
+  },
+  {
+    name: "Profile",
+    layout: "/admin",
+    path: "profile",
+    icon: <MdPerson className="w-6 h-6" />,
+    component: <Profile />,
+  },
+  {
+    name: "Sign In",
+    layout: "/auth",
+    path: "sign-in",
+    icon: <MdLock className="w-6 h-6" />,
+    component: <SignIn />,
+  },
+  {
+    name: "RTL Admin",
+    layout: "/rtl",
+    path: "rtl",
+    icon: <MdHome className="w-6 h-6" />,
+    component: <RTLDefault />,
+  },
+];
+export default routes;
