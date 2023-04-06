95b19e9eda3ca157dfbf2aec8e908ab60ce75540
shabeeb <shabeebkaip@gmail.com>
2023-04-06 16:35:17 +0530

    sign in changes

--- src/components/footer/FooterAuthDefault.jsx
+++ src/components/footer/FooterAuthDefault.jsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="z-[5] mx-auto flex w-full max-w-screen-sm flex-col items-center justify-between px-[20px] pb-4 lg:mb-6 lg:max-w-[100%] lg:flex-row xl:mb-2 xl:w-[1310px] xl:pb-6">
-      <p className="mb-6 text-center text-sm text-gray-600 md:text-base lg:mb-0">
-        ©{1900 + new Date().getYear()} Horizon UI. All Rights Reserved.
+      <p className="mb-6 text-sm text-center text-gray-600 md:text-base lg:mb-0">
+        ©{1900 + new Date().getYear()} Code-Ox UI. All Rights Reserved.
       </p>
       <ul className="flex flex-wrap items-center sm:flex-nowrap">
         <li className="mr-12">
           <a
             target="blank"
-            href="mailto:hello@simmmple.com"
+            href="mailto:mailk@code-ox.com"
             className="text-sm text-gray-600 hover:text-gray-600 md:text-base lg:text-white lg:hover:text-white"
           >
             Support
@@ -18,8 +18,8 @@ export default function Footer() {
         </li>
         <li className="mr-12">
           <a
-            target="blank"
-            href="https://simmmple.com/licenses"
+            target="_blank"
+            href="https://code-ox.com/"
             className="text-sm text-gray-600 hover:text-gray-600 md:text-base lg:text-white lg:hover:text-white"
           >
             License
@@ -28,7 +28,7 @@ export default function Footer() {
         <li className="mr-12">
           <a
             target="blank"
-            href="https://simmmple.com/terms-of-service"
+            href="https://code-ox.com/"
             className="text-sm text-gray-600 hover:text-gray-600 md:text-base lg:text-white lg:hover:text-white"
           >
             Terms of Use
@@ -37,7 +37,7 @@ export default function Footer() {
         <li>
           <a
             target="blank"
-            href="https://blog.horizon-ui.com/"
+            href="https://code-ox.com/"
             className="text-sm text-gray-600 hover:text-gray-600 md:text-base lg:text-white lg:hover:text-white"
           >
             Blog
